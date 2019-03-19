2b77add6a2957577adfa7b487980970ed663da9f
alxshelepenok <alxshelepenok@gmail.com>
2019-03-19 14:29:47 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "MMTrade Status",
+  "name": "mmtrade_status",
+  "description": "",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
---
+++ content/2019-03-19_systems-are-down.md
@@ -0,0 +1,11 @@
+---
+title: Systems are down
+date: '2019-03-19T11:01:00.303Z'
+severity: partial-outage
+affectedsystems:
+  - api
+resolved: true
+---
+We will be deploying the next version.
+
+<!--- language code: en -->
